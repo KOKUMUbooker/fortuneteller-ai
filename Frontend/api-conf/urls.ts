dcec2f36dae232e82a4bb2aeef5b5f665bd8ad4e
KOKUMUbooker <72650199+KOKUMUbooker@users.noreply.github.com>
2026-02-01 20:14:36 +0300

    Update API_BASE_URL

--- Frontend/api-conf/urls.ts
+++ Frontend/api-conf/urls.ts
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = "http://localhost:8080";
-export const API_BASE_URL ="https://fortuneteller-ai-1.onrender.com";
+export const API_BASE_URL ="https://fortuneteller-ai-3ogr.onrender.com";
